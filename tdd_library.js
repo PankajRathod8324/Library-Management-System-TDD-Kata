d31b3866e9aee043addc73f9bbb1c82dbbac6511
PankajRathod8324 <96436975+PankajRathod8324@users.noreply.github.com>
2024-12-28 23:31:29 +0530

    Add test for adding books testcase - 2 : Green Phase

--- tdd_library.js
+++ tdd_library.js
@@ -4,6 +4,17 @@ class Library {
     }
 
     addBook(book) {
+        const isduplicate = this.books.some((e) => 
+            e.isbn === book.isbn &&
+            e.title === book.title &&
+            e.author === book.author &&
+            e.year === book.year 
+        );
+
+        if (isduplicate) {
+            throw new Error('Book with these details are already exists.');
+        }
+        
         this.books.push({ ...book, available: true });
     }
 }
