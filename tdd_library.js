101a1351b12dffb3a2fd8272dc59418735c3623a
PankajRathod8324 <96436975+PankajRathod8324@users.noreply.github.com>
2024-12-29 01:30:07 +0530

    Add test for returning books testcase - 1 : Green Phase

--- tdd_library.js
+++ tdd_library.js
@@ -26,6 +26,20 @@ class Library {
         }
         book.available = false;
     }
+    
+    returnBook(isbn){
+        const book = this.books.find((e) => e.isbn === isbn);
+
+        if(!book) {
+            throw new Error('The book is not found');
+        }
+        if(book.available) {
+            throw new Error('The book is not currently borrowed.');
+        }
+        book.available = true;
+    }
+
+
 }
 
 export default Library;
